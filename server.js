0bbd22b0717a2cbc0167fccb72c8720916c49fc1
Gautam J <gautam.jayapal@gmail.com>
2020-12-07 20:36:18 +0530

    add middlewares

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan("dev")); // logger
-// :method :url :status :response-time ms - :res[content-length]
+// Initialize Middlewares
+// logger => :method :url :status :response-time ms - :res[content-length]
+app.use(morgan("dev"));
+// parse incoming requests with JSON payloads
+app.use(express.json());
 
+// default empty route to check API
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// either PORT env variable or 5000
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`REST API running on port ${PORT}`));
+app.listen(PORT, () => console.log(`REST API running on port ${PORT}...`));
